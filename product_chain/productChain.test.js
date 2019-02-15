d7ec546d39cafd4fdf7f0bf70cafc901c1bade8f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-15 20:43:29 +0100

    Modify the productionChain.test.js file to accommodate the decoupling of the producer class

--- product_chain/productChain.test.js
+++ product_chain/productChain.test.js
@@ -1,34 +1,41 @@
 //Import productChain class from productChain.js
-const ProductManager = require('./productChain');
+const ProductManager = require('../product_chain/producer/producer');
+const ProductChain = require('./productChain');
 
 
 
 const mockProductChannel = jest.fn();
-jest.mock('./productChain', () => {
-    return jest.fn().mockImplementation(() =>{
-        return {makeChannel:mockProductChannel};
+jest.mock('./producer/producer', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            makeChannel: mockProductChannel
+        };
     });
 });
 
-beforeEach(() =>{
+beforeEach(() => {
     ProductManager.mockClear();
     mockProductChannel.mockClear();
 });
-
-it('The channel should be able to call new() on ProductChain', () => {
-    //Ensure the constructor created the object:
-    const producer = new ProductManager();
-    expect(producer).toBeTruthy();
+describe('Check if the channel called new() on ProductionChain', () => {
+    it('The channel should be able to call new() on ProductChain', () => {
+        //Ensure the constructor created the object:
+        const producer = new ProductManager();
+        expect(producer).toBeTruthy();
+    });
 });
 
-it('We can check if the producer called the class constructor',() => {
-    const producer = new ProductManager();
-    expect(ProductManager).toHaveBeenCalledTimes(1);
+describe('Check', () => {
+    it('We can check if the producer called the class constructor', () => {
+        const producer = new ProductManager();
+        expect(ProductManager).toHaveBeenCalledTimes(1);
+    });
 });
 
-it('We can check if the producer called a method on the class instance', () => {
-    const producer = new ProductManager();
-    const channelCreate = 'I am a producer';
-    producer.makeChannel();
-    expect(mockProductChannel.mock.calls[0][0]).toEqual(channelCreate);
-});
+
+// it('We can check if the producer called a method on the class instance', () => {
+//     const producer = new ProductManager();
+//     const channelCreate = 'I am a producer';
+//     producer.createProductChannel();
+//     expect(mockProductChannel.mock.calls[0][0]).toEqual(channelCreate);
+// });
