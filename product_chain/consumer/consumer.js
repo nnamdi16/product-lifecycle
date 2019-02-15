b7f64acf2492273847a35226b8b5bfcf44590233
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-15 21:48:32 +0100

    Create consumer class and a Consumer manager sub class

--- product_chain/consumer/consumer.js
+++ product_chain/consumer/consumer.js
@@ -1,2 +1,17 @@
+const ProductChain = require('../productChain');
 class Consumer {
+    createChannel () {
+        console.log('I am a consumer');
+    }
 }
+
+class ConsumerManager extends ProductChain {
+    makeChannel () {
+        return new Consumer();
+    }
+}
+
+const devConsumer = new ConsumerManager();
+devProducer.createProductChannel();
+
+module.exports = ConsumerManager;
