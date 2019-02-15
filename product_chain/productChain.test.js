a883f8271e31cc3b66269d84ceb33614f4d1c07d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-15 19:29:50 +0100

    Create test cases to test classes created in productChain.js

--- product_chain/productChain.test.js
+++ product_chain/productChain.test.js
@@ -1,29 +1,34 @@
 //Import productChain class from productChain.js
-const ProductChain = require('./productChain');
+const ProductManager = require('./productChain');
+
+
 
 const mockProductChannel = jest.fn();
 jest.mock('./productChain', () => {
     return jest.fn().mockImplementation(() =>{
-        return {productChannel:mockProductChannel};
+        return {makeChannel:mockProductChannel};
     });
 });
 
 beforeEach(() =>{
-    ProductChain.mockClear();
+    ProductManager.mockClear();
     mockProductChannel.mockClear();
 });
 
 it('The channel should be able to call new() on ProductChain', () => {
     //Ensure the constructor created the object:
-    const producer = new ProductChain();
+    const producer = new ProductManager();
     expect(producer).toBeTruthy();
 });
 
 it('We can check if the producer called the class constructor',() => {
-    const producer = new ProductChain();
-    expect(producer).toHaveBeenCalledTimes(1);
+    const producer = new ProductManager();
+    expect(ProductManager).toHaveBeenCalledTimes(1);
 });
 
-// it('We can check if the producer called a method on the class instance', () => {
-//     const producer = new ProductChain();
-// });
+it('We can check if the producer called a method on the class instance', () => {
+    const producer = new ProductManager();
+    const channelCreate = 'I am a producer';
+    producer.makeChannel();
+    expect(mockProductChannel.mock.calls[0][0]).toEqual(channelCreate);
+});
