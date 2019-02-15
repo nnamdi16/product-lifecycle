19d95b672e8e74a2c87d78cf5e673839a116328e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-15 21:47:25 +0100

    Decouple the retailer, consumer and recycler classes

--- product_chain/productChain.js
+++ product_chain/productChain.js
@@ -1,5 +1,3 @@
-
-
 class ProductChain {
     constructor(product) {
         this.product = product;
@@ -12,53 +10,5 @@ class ProductChain {
 
 }
 
-
-class Retailer {
-    createChannel() {
-        console.log('I am a retailer');
-    }
-}
-
-class Consumer {
-    createChannel () {
-        console.log('I am a consumer');
-    }
-}
-
-class Recycler {
-    createChannel () {
-        console.log('I am a recycler');
-    }
-}
-
-// class ProductManager extends ProductChain {
-//     makeChannel () {
-//         return new Producer();
-//     }
-// }
-
-class RetailerManager extends ProductChain {
-    makeChannel () {
-        return new Retailer();
-    }
-}
-
-class ConsumerManager extends ProductChain {
-    makeChannel () {
-        return new Consumer();
-    }
-}
-
-class RecyclerManager extends ProductChain {
-    makeChannel () {
-        return new Recycler();
-    }
-}
-
-// const devProducer = new ProductManager();
-// devProducer.createProductChannel();
-
-const devRetailer = new RetailerManager();
-devRetailer.createProductChannel();
 // module.exports = ProductManager;
 module.exports = ProductChain;
