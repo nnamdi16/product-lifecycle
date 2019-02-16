11b43a5fb9f642dc811c9cd5e6cf82e7aa5bff62
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-16 22:11:27 +0100

    Redesign the production chain class in line with state design pattern

--- product_chain/productChain.js
+++ product_chain/productChain.js
@@ -1,19 +1,40 @@
+const fs = require('fs');
+// let lyrics = 'I am reading a book';
+const Producer = require('./producer/producer');
+const Retailer = require('./retailer/retailer');
+const Consumer = require('./consumer/consumer');
+const Recycler = require('./recycler/recycler');
+// fs.writeFile('../data/output-file.txt',lyrics,(err) => {
+//     if(err) throw err;
+//     console.log('Lyrics Saved!');
+// });
 class ProductChain {
-    constructor(product) {
+    constructor(product,state) {
         this.product = product;
-    }
-    createProductChannel(){
-            const productChannel = this.makeChannel();
-            productChannel.createChannel();
-    }
+        this.currentState = state === 'Producer' ? new Producer(this) : (state === 'Retailer' ? new Retailer(this):(state === 'Consumer' ? new Consumer(this): new Recycler(this))); 
 
-    moveProduct () {
-        return this.product;
     }
+    nextState(state) {
+        this.currentState = state;
+        this.currentState.go(this.product);
+        return true;
+    }
+    start() {
+        this.currentState.go(this.product);
+        return true;
+    }
+    // createProductChannel(){
+    //         const productChannel = this.makeChannel();
+    //         productChannel.createChannel();
+    // }
+
+    // moveProduct () {
+    //     return this.product;
+    // }
 
 
 }
-const productChain = new ProductChain('Calista Na');
-console.log(productChain)
+// const productChain = new ProductChain('Calista Na');
+// console.log(productChain);
 // module.exports = ProductManager;
 module.exports = ProductChain;
