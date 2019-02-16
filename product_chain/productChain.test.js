81568f0a19e24c4cf1ad5c77f7062128b3901057
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-16 09:06:14 +0100

    Modify the productChain.test.js file including the file path for mock testing, method and the class

--- product_chain/productChain.test.js
+++ product_chain/productChain.test.js
@@ -1,36 +1,43 @@
 //Import productChain class from productChain.js
-const ProductManager = require('../product_chain/producer/producer');
+// const ProductManager = require('../product_chain/producer/producer');
 const ProductChain = require('./productChain');
 
 
 
 const mockProductChannel = jest.fn();
-jest.mock('./producer/producer', () => {
+jest.mock('./productChain', () => {
     return jest.fn().mockImplementation(() => {
         return {
-            makeChannel: mockProductChannel
+            createProductChannel: mockProductChannel
         };
     });
 });
 
 beforeEach(() => {
-    ProductManager.mockClear();
+    ProductChain.mockClear();
     mockProductChannel.mockClear();
 });
 describe('Check if the channel called new() on ProductionChain', () => {
     it('The channel should be able to call new() on ProductChain', () => {
         //Ensure the constructor created the object:
-        const producer = new ProductManager();
-        expect(producer).toBeTruthy();
+        const product = new ProductChain();
+        expect(product).toBeTruthy();
     });
 });
 
 describe('Check', () => {
     it('We can check if the producer called the class constructor', () => {
-        const producer = new ProductManager();
-        expect(ProductManager).toHaveBeenCalledTimes(1);
+        const product = new ProductChain();
+        expect(ProductChain).toHaveBeenCalledTimes(1);
     });
 });
+// describe('Check', () => {
+//     it('Should create an instance of the ProductChain class', () => {
+//         const product = new ProductChain('Calista Na');
+//         const result = {'product':'Calista Na'};
+//         expect(product).toEqual(result);
+//     });
+// });
 
 
 // it('We can check if the producer called a method on the class instance', () => {
