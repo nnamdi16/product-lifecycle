3b3e3fc084ad1f965042d312e75be691fcf6da06
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-16 22:17:07 +0100

    Remodify the recycler  class, adding the go method that instantiates the next class

--- product_chain/recycler/recycler.js
+++ product_chain/recycler/recycler.js
@@ -1,16 +1,28 @@
-
 const ProductChain = require('../productChain');
-class Recycler {
-    createChannel () {
-        console.log('I am a recycler');
+const Producer = require('../producer/producer');
+class Recycler extends ProductChain{
+    constructor(productChain) {
+        this.productChain = productChain;
     }
+    go() {
+        let output = 'Product moved from Recycler to Producer';
+        fs.writeFile('../data/output-file.txt',output,(err) => {
+            if(err) throw err;
+            console.log('Product processed from Retailer!');
+        });
+        // productChain.nextState(new Producer(productChain));
+        
+    }
+    // createChannel () {
+    //     console.log('I am a recycler');
+    // }
 }
 
 
-class RecyclerManager extends ProductChain {
-    makeChannel () {
-        return new Recycler();
-    }
-}
+// class RecyclerManager extends ProductChain {
+//     makeChannel () {
+//         return new Recycler();
+//     }
+// }
 
-module.exports = RecyclerManager;
+module.exports = Recycler;
