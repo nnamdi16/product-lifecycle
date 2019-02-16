3b2aa9ff2bfbf07cb40dfe58a997126bc3dfc0e8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-16 22:14:55 +0100

    Remodify the consumer class, adding the go method that instantiates the next class

--- product_chain/consumer/consumer.js
+++ product_chain/consumer/consumer.js
@@ -1,17 +1,37 @@
 const ProductChain = require('../productChain');
-class Consumer {
-    createChannel () {
-        console.log('I am a consumer');
+const Recycler = require('../recycler/recycler');
+const fs = require('fs');
+// let lyrics = 'I am reading a book';
+
+
+class Consumer extends ProductChain {
+    constructor(productChain){
+        this.productChain = productChain;
     }
+    
+    go() {
+        let output = 'Product moved from Consumer to Recycler';
+        fs.writeFile('../data/output-file.txt',output,(err) => {
+            if(err) throw err;
+            console.log('Product processed from Consumer!');
+        });
+        productChain.nextState(new Recycler(productChain));
+        
+    }
+    // createChannel () {
+    //     console.log('I am a consumer');
+    // }
 }
 
-class ConsumerManager extends ProductChain {
-    makeChannel () {
-        return new Consumer();
-    }
-}
+// class ConsumerManager extends ProductChain {
+//     makeChannel () {
+//         return new Consumer();
+//     }
+// }
 
-const devConsumer = new ConsumerManager();
-devProducer.createProductChannel();
+// const devConsumer = new ConsumerManager();
+// devProducer.createProductChannel();
 
-module.exports = ConsumerManager;
+// module.exports = ConsumerManager;
+
+module.exports = Consumer;
