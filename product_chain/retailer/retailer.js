e82e33f49234011880d23e4f759ac243e8a8840d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-16 22:17:44 +0100

    Remodify the retailer class, adding the go method that instantiates the next class

--- product_chain/retailer/retailer.js
+++ product_chain/retailer/retailer.js
@@ -1,18 +1,28 @@
 const ProductChain = require('../productChain');
-class Retailer {
-    createChannel() {
-        console.log('I am a retailer');
+const Consumer = require('../consumer/consumer');
+class Retailer extends ProductChain{
+    constructor(productChain) {
+        this.productChain = productChain;
+    }
+    go() {
+        let output = 'Product moved from Retailer to Consumer';
+        fs.writeFile('../data/output-file.txt',output,(err) => {
+            if(err) throw err;
+            console.log('Product processed from Retailer!');
+        });
+        productChain.nextState(new Consumer(productChain));
+        
     }
 }
 
 
-class RetailerManager extends ProductChain {
-    makeChannel () {
-        return new Retailer();
-    }
-}
+// class RetailerManager extends ProductChain {
+//     makeChannel () {
+//         return new Retailer();
+//     }
+// }
 
-const devRetailer = new RetailerManager();
-devRetailer.createProductChannel();
+// const devRetailer = new RetailerManager();
+// devRetailer.createProductChannel();
 
-module.exports = RetailerManager;
+module.exports = Retailer;
