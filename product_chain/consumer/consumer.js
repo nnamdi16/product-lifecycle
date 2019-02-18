bfefc17b959e800a8757111748fa51fb1fc0c94b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-18 22:07:54 +0100

    Remove redundant lines of code

--- product_chain/consumer/consumer.js
+++ product_chain/consumer/consumer.js
@@ -4,47 +4,24 @@ const Recycler = require('../recycler/recycler');
 const readFile = require('../entry_point/readData');
 const Producer = require('../producer/producer');
 
-// let lyrics = 'I am reading a book';
-// const content = 'Some content!';
-
-// fs.appendFile('../data/output-file.txt', '\n'+ content, (err) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   //done!
-// });
 
 class Consumer {
     constructor(readFile){
         this.readFile = readFile;
-        // this.productChannel = new ProductChain();
     }
     
-    move() {
-        let output = `Product moved from Consumer to Recycler`;
-        fs.appendFile('../data/output-file.txt','\n' + output,(err) => {
+    move(prop) {
+        let output = `${prop} Product moved from Consumer to Recycler`;
+        fs.appendFile('../../data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
-            console.log('Product processed from Consumer!');
+            console.log(`${prop} Product processed from Consumer!`);
             return;
         });
         this.readFile.nextState(new Recycler(readFile));
         
     }
-    // createChannel () {
-    //     console.log('I am a consumer');
-    // }
+    
 }
 
-// class ConsumerManager extends ProductChain {
-//     makeChannel () {
-//         return new Consumer();
-//     }
-// }
-
-// const devConsumer = new ConsumerManager();
-// devProducer.createProductChannel();
-
-// module.exports = ConsumerManager;
 
 module.exports = Consumer;
