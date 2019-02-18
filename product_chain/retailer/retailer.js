6d06aa7712bd506018d9283c1d09a6e1e954750b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-18 22:15:02 +0100

    Modify the retailer.js file, removing redundant lines of code and creating a parameter that is passed to the appendFile method

--- product_chain/retailer/retailer.js
+++ product_chain/retailer/retailer.js
@@ -1,31 +1,30 @@
 const fs  = require('fs');
+
 const ProductChain = require('../productChain');
 const Consumer = require('../consumer/consumer');
 const readFile = require('../entry_point/readData');
+const Producer = require('../producer/producer');
 class Retailer {
-    constructor(readFile) {
-        this.readFile = readFile;
+    constructor(products) {
+        this.products = products;
+
     }
-    move() {
-        let output = 'Product moved from Retailer to Consumer';
-        fs.appendFile('../data/output-file','\n' + output,(err) => {
+    move(prop) {
+
+        let output = `${prop} Product moved from Retailer to Consumer`;
+        fs.appendFile('../../data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
-            console.log('Product processed from Retailer to Consumer!');
+            console.log(`${prop} Product processed from Retailer to Consumer!`);
             return;
         });
-        this.readFile.nextState(new Consumer(readFile));
+        
+        // this.readFile.nextState(new Consumer(readFile));
         
     }
 }
 
 
-// class RetailerManager extends ProductChain {
-//     makeChannel () {
-//         return new Retailer();
-//     }
-// }
-
-// const devRetailer = new RetailerManager();
-// devRetailer.createProductChannel();
+// const retailer = new  Retailer();
+// console.log(retailer);
 
 module.exports = Retailer;
