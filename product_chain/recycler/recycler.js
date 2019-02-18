1fb178746f1b7746cc4a1ffe75c1ee26a92cf4e7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-18 22:13:17 +0100

    Modify the recycler.js file, removing redundant lines of code and creating a parameter that is passed to the appendFile method

--- product_chain/recycler/recycler.js
+++ product_chain/recycler/recycler.js
@@ -9,26 +9,20 @@ class Recycler  {
         // this.product = product;
         
     }
-    move() {
-        let output = `Product moved from Recycler to Producer`;
-        fs.appendFile('../data/output-file','\n' + output,(err) => {
+    move(prop) {
+        let output = `${prop} Product moved from Recycler to Producer`;
+        fs.appendFile('../../data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
-            console.log('Product processed from Recycler!');
+            console.log(`${prop} Product processed from Recycler!`);
             return;
         });
         // this.readFile.nextState(new Producer(readFile));
         
     }
-    // createChannel () {
-    //     console.log('I am a recycler');
-    // }
+    
 }
 
 
-// class RecyclerManager extends ProductChain {
-//     makeChannel () {
-//         return new Recycler();
-//     }
-// }
+
 
 module.exports = Recycler;
