36823e5669369e1a8d6484c0ae45409898184bbf
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-18 01:38:34 +0100

    Change the file path of readFile and rename the method from go to Move

--- product_chain/retailer/retailer.js
+++ product_chain/retailer/retailer.js
@@ -1,14 +1,14 @@
 const fs  = require('fs');
 const ProductChain = require('../productChain');
 const Consumer = require('../consumer/consumer');
-const readFile = require('../../data/readData');
+const readFile = require('../entry_point/readData');
 class Retailer {
     constructor(readFile) {
         this.readFile = readFile;
     }
-    go() {
+    move() {
         let output = 'Product moved from Retailer to Consumer';
-        fs.appendFile('../data/output-file.txt','\n' + output,(err) => {
+        fs.appendFile('../data/output-file','\n' + output,(err) => {
             if(err) throw err;
             console.log('Product processed from Retailer to Consumer!');
             return;
