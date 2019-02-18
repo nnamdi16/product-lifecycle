f9683ca064356ecd956201666c0f6cd627c977bf
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-18 22:11:58 +0100

    Modify the producer.js file, removing redundant lines of code and creating a parameter that is passed to the appendFile method

--- product_chain/producer/producer.js
+++ product_chain/producer/producer.js
@@ -3,83 +3,25 @@ const ProductChain = require('../productChain');
 const Retailer = require('../retailer/retailer');
 const readFile = require('../entry_point/readData');
 const Consumer = require('../consumer/consumer');
-// const producerStation = require('../../data/producerStation');
-// const producerMove = require('../../data/producerMove');
-// const ProductState = require('../../util/productState');
+
 class Producer {
     constructor(readFile) {
         this.readFile = readFile;
         
+        
     }
-    move() {
-        let output = `Product moved from Producer to Retailer`;
-        fs.appendFile('../data/output-file','\n' + output,(err) => {
+    move(prop) {
+        let output = `${prop} Product moved from Producer to Retailer`;
+        //  console.log(readFile.product);
+        fs.appendFile('../../data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
-            console.log('Product processed from Producer!');
+            console.log(`${prop} Product processed from Producer!`);
             return;
         });
         this.readFile.nextState(new Retailer(readFile));
         
     }
-    // createChannel() {
-    //     console.log('I am a producer');
-    // }
+ 
 }
 
-// class ProductManager extends ProductChain {
-//     constructor() {
-//         super();
-//         this.productState = new ProductState (); //Creating an Object type productState so as to access its methods
-//         // console.log('this.productState: ', this.productState);
-//     }
-//     makeChannel() {
-//         return new Producer();
-//     }
-
-//     receiveProduct(productName) {
-//         if(producerStation.length > 0)
-//         producerMove.push({'ProductName': productName});
-//          else producerStation.push({
-//             'ProductName': productName
-//         });
-        
-//     }
-
-//     sendProduct() {
-//         let productSend =  this.productState.productMove();
-//         return productSend;
-//     }
-
-//     changeOperation(){
-//         this.productState.productMove();
-        
-//     }
-    
-// }
-
-
-
-// const devProducer = new ProductManager('Calista Na');
-// const damProduce = new ProductState('Classic');
-// // // console.log('damProduce: ', damProduce);
-// damProduce.productMove();
-// // console.log('damProduce.productMove();: ', damProduce.productMove());
-// damProduce.change();
-// damProduce.productMove();
-// // damProduce.change();
-// // damProduce.change();
-// console.log('damProduce.productMove();: ', damProduce.productMove());
-// // console.log('devProducer: ', devProducer);
-
-// // devProducer.createProductChannel();
-// // devProducer.receiveProduct('Calista Na');
-
-// // console.log('Producer Station',producerStation);
-// // devProducer.sendProduct();
-// // console.log('devProducer.moveProduct();: ', devProducer.sendProduct());
-
-// // console.log('New Dev',devProducer);
-
-
-// module.exports = ProductManager;
 module.exports = Producer;
