3767eee6df26e7e2ef2d662f0c25df099e5b6b8e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-19 10:12:25 +0100

    Change the naming of the variable ReadFile to ExecuteOperation, other variable names as well as productInput file path

--- product_cycle/product_chain/producer/producer.js
+++ product_cycle/product_chain/producer/producer.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const ProductChain = require('../productChain');
 const Retailer = require('../retailer/retailer');
-const readFile = require('../entry_point/readData');
+const ExecuteOperation = require('../../app');
 const Consumer = require('../consumer/consumer');
 
 class Producer {
-    constructor(readFile) {
-        this.readFile = readFile;
+    constructor(executeOperation) {
+        this.executeOperation = executeOperation;
         
         
     }
     move(prop) {
         let output = `${prop} Product moved from Producer to Retailer`;
         //  console.log(readFile.product);
-        fs.appendFile('../entry_point/output-file.txt','\n' + output,(err) => {
+        fs.appendFile('../../../data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
             console.log(`${prop} Product processed from Producer!`);
             return;
         });
-        this.readFile.nextState(new Retailer(readFile));
+        // this.executeOperation.nextState(new Retailer(executeOperation));
         
     }
  
