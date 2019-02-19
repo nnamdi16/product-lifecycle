5e7aa9a2f48a4f5c853e9cc342767c841d5cb5d5
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-19 09:12:10 +0100

    Rename folders containing all the file

---
+++ product_cycle/product_chain/productChain.test.js
@@ -0,0 +1,59 @@
+//Import productChain class from productChain.js
+// const ProductManager = require('../product_chain/producer/producer');
+const ProductChain = require('./productChain');
+const readFile = require('./entry_point/readData');
+
+
+
+const mockProductChain = jest.fn();
+jest.mock('./productChain', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            start: mockProductChain
+        };
+    });
+});
+
+beforeEach(() => {
+    ProductChain.mockClear();
+    mockProductChain.mockClear();
+});
+describe('Check if the channel called new() on ProductionChain', () => {
+    it('The channel should be able to call new() on ProductChain', () => {
+        //Ensure the constructor created the object:
+        const product = new ProductChain();
+        expect(product).toBeTruthy();
+    });
+});
+
+describe('Check', () => {
+    it('We can check if the producer called the class constructor', () => {
+        const product = new ProductChain();
+        expect(ProductChain).toHaveBeenCalledTimes(1);
+    });
+});
+
+// it('works', () => {
+//     const produce = new ProductChain('Lucas Menefee', 'Producer');
+//     const result = {
+//         product:'Lucas Menefee',
+//         state: 'Consumer'
+//     }
+//     expect(result).toBe(produce);
+    
+//   });
+// describe('Check', () => {
+//     it('Should create an instance of the ProductChain class', () => {
+//         const product = new ProductChain('Calista Na');
+//         const result = {'product':'Calista Na'};
+//         expect(product).toEqual(result);
+//     });
+// });
+
+
+// it('We can check if the producer called a method on the class instance', () => {
+//     const producer = new ProductManager();
+//     const channelCreate = 'I am a producer';
+//     producer.createProductChannel();
+//     expect(mockProductChannel.mock.calls[0][0]).toEqual(channelCreate);
+// });
---
+++ product_cycle/product_chain/retailer/retailer.js
@@ -0,0 +1,30 @@
+const fs  = require('fs');
+
+const ProductChain = require('../productChain');
+const Consumer = require('../consumer/consumer');
+const readFile = require('../entry_point/readData');
+const Producer = require('../producer/producer');
+class Retailer {
+    constructor(products) {
+        this.products = products;
+
+    }
+    move(prop) {
+
+        let output = `${prop} Product moved from Retailer to Consumer`;
+        fs.appendFile('../entry_point/output-file.txt','\n' + output,(err) => {
+            if(err) throw err;
+            console.log(`${prop} Product processed from Retailer to Consumer!`);
+            return;
+        });
+        
+        // this.readFile.nextState(new Consumer(readFile));
+        
+    }
+}
+
+
+// const retailer = new  Retailer();
+// console.log(retailer);
+
+module.exports = Retailer;
