f213d479c5c046acaf83965212b71df95c68c9ac
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-02-28 15:41:12 +0100

    Modify the file architecture

--- product_cycle/app.js
+++ product_cycle/app.js
@@ -3,65 +3,75 @@ const fs = require('fs');
 class ExecuteOperation {
 
     execute() {
-        fs.readFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/product-Input.txt', 'utf-8', function (err, contents) {
-            let productArray = [];
-            if (err) throw err;
-            let array = contents.toString().split("\n");
-            // console.log(array);
-
-             array.forEach(element => {
-               let result = element.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\t+\r?\n|\r]/gm, ' ').replace(/ +/gm, ' ').split(' ');
-            //    return result;
-                let len = result.length;
-                    let prop = `${result[0]} ${result[1]}`;
-                    let state = `${result[2]}`;
-                    // console.log(`${result[0]} ${result[1]} ${result[2]}`);
-                    if(len > 2) {
-                        const productChain  = new ProductChain(prop, state);
-                        productChain.start();
-                    } else if (len === 2 && result[0] !== '' )  {
-                       const productChain = new ProductChain(prop, 'Producer');
-                       productChain.start();
-                    }
-                // console.log(result);
-            });
-            // for (let i in array) {
-            //     let reg = /[&\/\\#,+()$~%.'":*?<>{}\t]/g;
-            //     let space = /\s\s+/g;
-            //     let result = array[i].replace(reg, '').replace(space,'').split('');
-            //     console.log(result);
-            //     // let len = result.length;
-            //     //     let prop = `${result[0]} ${result[1]}`;
-            //     //     let state = `${result[2]}`;
-            //     //     // console.log(`${result[0]} ${result[1]} ${result[2]}`);
-            //     //     if(len === 3) {
-            //     //         const productChain  = new ProductChain(prop, state);
-            //     //         productChain.start();
-            //     //     } else if (len === 2) {
-            //     //        const productChain = new ProductChain(prop, 'Producer');
-            //     //        productChain.start();
-            //     //     }
-            //     // if (!(reg.test(array[i])) && !(space.test(array[i]))) {
-            //     //     let result = array[i].split(" ");
-            //     //     productArray.push(result);
-            //     //     let len = result.length;
-            //     //     let prop = `${result[0]} ${result[1]}`;
-            //     //     let state = `${result[2]}`;
-            //     //     // console.log(`${result[0]} ${result[1]} ${result[2]}`);
-            //     //     if(len === 3) {
-            //     //         const productChain  = new ProductChain(prop, state);
-            //     //         productChain.start();
-            //     //     } else if (len === 2) {
-            //     //        const productChain = new ProductChain(prop, 'Producer');
-            //     //        productChain.start();
-            //     //     }
-            //     //     // console.log(productArray);
-            //     // }
-            //         // console.log(productArray);
-                 
-            // }
+        try {
+            const data = fs.readFileSync()
+        } catch (err) {
             
-        });
+        }
+        // fs.readFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/product-Input.txt', 'utf-8', function (err, contents) {
+        //     let productArray = [];
+        //     if (err) throw err;
+        //     let array = contents.toString().split("\n");
+        //     let count = 1;
+            
+        //     // console.log(array);
+            
+        //      array.forEach((element,i) => {
+        //         let result = element.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\t+\r?\n|\r]/gm, ' ').replace(/ +/gm, ' ');
+        //         let input = result.test(/^\s*$/gm);
+        //         if(!input) fs.appendFile
+        //     //    return result;
+        //         //  i++;
+        //         // let len = result.length;
+        //         //     let prop = `${i} ${result[0]} ${result[1]}`;
+        //         //     let state = `${result[2]}`;
+        //         //     // console.log(`${result[0]} ${result[1]} ${result[2]}`);
+        //         //     if(len > 2) {
+        //         //         const productChain  = new ProductChain(prop, state);
+        //         //         productChain.start();
+        //         //     } else if (len === 2 && result[0] !== '' )  {
+        //         //        const productChain = new ProductChain(prop, 'Producer');
+        //         //        productChain.start();
+        //         //     }
+        //         console.log(result);
+        //     });
+        //     // for (let i in array) {
+        //     //     let reg = /[&\/\\#,+()$~%.'":*?<>{}\t]/g;
+        //     //     let space = /\s\s+/g;
+        //     //     let result = array[i].replace(reg, '').replace(space,'').split('');
+        //     //     console.log(result);
+        //     //     // let len = result.length;
+        //     //     //     let prop = `${result[0]} ${result[1]}`;
+        //     //     //     let state = `${result[2]}`;
+        //     //     //     // console.log(`${result[0]} ${result[1]} ${result[2]}`);
+        //     //     //     if(len === 3) {
+        //     //     //         const productChain  = new ProductChain(prop, state);
+        //     //     //         productChain.start();
+        //     //     //     } else if (len === 2) {
+        //     //     //        const productChain = new ProductChain(prop, 'Producer');
+        //     //     //        productChain.start();
+        //     //     //     }
+        //     //     // if (!(reg.test(array[i])) && !(space.test(array[i]))) {
+        //     //     //     let result = array[i].split(" ");
+        //     //     //     productArray.push(result);
+        //     //     //     let len = result.length;
+        //     //     //     let prop = `${result[0]} ${result[1]}`;
+        //     //     //     let state = `${result[2]}`;
+        //     //     //     // console.log(`${result[0]} ${result[1]} ${result[2]}`);
+        //     //     //     if(len === 3) {
+        //     //     //         const productChain  = new ProductChain(prop, state);
+        //     //     //         productChain.start();
+        //     //     //     } else if (len === 2) {
+        //     //     //        const productChain = new ProductChain(prop, 'Producer');
+        //     //     //        productChain.start();
+        //     //     //     }
+        //     //     //     // console.log(productArray);
+        //     //     // }
+        //     //         // console.log(productArray);
+                 
+        //     // }
+            
+        // });
         
     }
 }
