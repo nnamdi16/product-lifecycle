8020e5d745857aed999f8d92fe750d921cc80846
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-01 17:10:39 +0100

    Create an empty constructor and remove executeOperation property

--- product_cycle/product_chain/retailer/retailer.js
+++ product_cycle/product_chain/retailer/retailer.js
@@ -1,15 +1,14 @@
 const fs  = require('fs');
 class Retailer {
-    constructor(executeOperation) {
-        this.executeOperation = executeOperation;
-
+    constructor() {
+        
     }
     async move(prop) {
 
         let output = `${prop} Product moved from Retailer to Consumer`;
         fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
-            this.executeOperation.nextState(new main.Consumer(this));
+           new main.Consumer().move(prop);
             console.log(`${prop} Product processed from Retailer to Consumer!`);
             return;
         });
