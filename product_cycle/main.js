305793c8805a1aeecba0db386d17c014f047b684
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-01 16:22:33 +0100

    Create a main file to accommaodate all module exports

---
+++ product_cycle/main.js
@@ -0,0 +1,7 @@
+module.exports.Consumer = require('../product_cycle/product_chain/consumer/consumer');
+module.exports.Producer = require('../product_cycle/product_chain/producer/producer');
+module.exports.Retailer = require('../product_cycle/product_chain/retailer/retailer');
+module.exports.Recycler = require('../product_cycle/product_chain/recycler/recycler');
+module.exports.ProductChain = require('../product_cycle/product_chain/productChain');
+module.exports.ExecuteOperation = require('../product_cycle/app');
+ 
