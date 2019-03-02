d0bbf168aefbe16fafd11e4e3b8c9b1d78f5d29f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-02 08:29:26 +0100

    Comment redundant code

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const ExecuteOperation = require('./product_cycle/app');
+// const Observable = require('./product_cycle/observable');
+// const main = require('./product_cycle/main');
+
 const executeOperation = new ExecuteOperation();
 executeOperation.execute();
+// const observable = new Observable();
+// const producer = new main.Producer();
+// let observer = observable.subscribe(main.producer.isLast);
+
