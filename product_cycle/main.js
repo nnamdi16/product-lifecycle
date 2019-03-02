975a4f03c05851a81e4a887b922111ba103b6f62
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-02 08:31:49 +0100

    Add Obeservable and ProductChainFactory classes required for export

--- product_cycle/main.js
+++ product_cycle/main.js
@@ -4,4 +4,6 @@ module.exports.Retailer = require('../product_cycle/product_chain/retailer/retai
 module.exports.Recycler = require('../product_cycle/product_chain/recycler/recycler');
 module.exports.ProductChain = require('../product_cycle/product_chain/productChain');
 module.exports.ExecuteOperation = require('../product_cycle/app');
+module.exports.Observable = require('./observable');
+module.exports.ProductChainFactory = require('./product_chain/productChainFactory');
  
