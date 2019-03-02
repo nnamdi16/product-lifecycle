85269aae8c8b3fbc5442b4a9a2f7bc291636a674
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-02 08:30:23 +0100

    Change variable name in the for loop

--- product_cycle/app.js
+++ product_cycle/app.js
@@ -6,9 +6,9 @@ class ExecuteOperation {
         try {
             const data = fs.readFileSync('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/productFlow.txt','utf8');
             let array = data.trim().split('\n');
-            for (let i = 0, len = array.length; i < len; i++) {
-                let product = (array[i].split(' '));
-                let prop = `${i} ${product[0]} ${product[1]}`;
+            for (let index = 0, len = array.length; index < len; index++) {
+                let product = (array[index].split(' '));
+                let prop = `${index} ${product[0]} ${product[1]}`;
                 let state = `${product[2]}`;
                 if(product.length === 3) {
                     const productChain = new main.ProductChain(state,prop);
