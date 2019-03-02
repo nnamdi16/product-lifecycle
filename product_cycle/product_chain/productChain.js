c5e40a25bf6cc35a96a4a1f8b97b22933daa0d7d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-02 08:34:57 +0100

    Refactor the code using Factory design pattern, registering various state category and assigning currentState to the instance of the state class category

--- product_cycle/product_chain/productChain.js
+++ product_cycle/product_chain/productChain.js
@@ -1,24 +1,26 @@
+//Required node fs module.
 const fs = require('fs');
 
+//Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
     constructor(state, product) {
         this.product = product; 
         this.state = state;
-        if (state === 'Producer') {
-            this.currentState = new main.Producer();
-        } else if (state === 'Retailer') {
-            this.currentState = new main.Retailer();
-        } else if(state === 'Consumer'){
-            this.currentState = new main.Consumer();
-        } else if(state === 'Retailer'){
-            this.currentState = new main.Recycler();
-        }else{
 
-        }
+        //Instantiated the productChain Factory that generates objects depending on the state change category.
+        const productChainFactory = new main.ProductChainFactory();
 
+        //Register the various state class category.
+        productChainFactory.register('Producer',main.Producer);
+        productChainFactory.register('Retailer',main.Retailer);
+        productChainFactory.register('Consumer',main.Consumer);
+        productChainFactory.register('Recycler',main.Recycler);
+
+        this.currentState = productChainFactory.getProductLink(state);
 
     }
 
+    //Method that ensures product flows from one state to the other.
     start() {
         this.currentState.move(this.product);
         return true;
