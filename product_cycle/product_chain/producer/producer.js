812a19f7095e85a8d28f108c110385f8b525a99c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-04 09:05:14 +0100

    Refactor isLast line of code

--- product_cycle/product_chain/producer/producer.js
+++ product_cycle/product_chain/producer/producer.js
@@ -8,13 +8,16 @@ class Producer {
         fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
             if(err) throw err;
             new main.Retailer().move(prop);
+            // this.isLast(prop);
+            // console.log(sample);
             console.log(`${prop} Product processed from Producer!`);
             return;
         });
     }
 
-    // isLast() {
-    //     return;
+    // isLast(prop) {
+    //     console.log('The Prop',prop);
+    //     if(prop ==='18599 Rosetta Fagen Producer') throw new Error('Product Cycle Completed ' + prop);
     // }
  
 }
