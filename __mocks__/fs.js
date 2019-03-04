33ef89a70c53e6fca205c08946cb0675d8ad429f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-04 13:45:54 +0100

    Uncomment redundant code

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,11 +1,11 @@
-const path = require('../product_cycle/app');
+// const path = require('../product_cycle/app');
 
-// jest.mock('../product_cycle/data/productFlow.txt','utf8',() => () =>({
+// // jest.mock('../product_cycle/data/productFlow.txt','utf8',() => () =>({
    
-// }));
+// // }));
 
-const fs = jest.mock('fs');
-const output = `Calista Na Product processed from Producer`;
+// const fs = jest.mock('fs');
+// const output = `Calista Na Product processed from Producer`;
 let appendFile = ('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
     if(err) throw err;
     return output;
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // const ExecuteOperation = require('./product_cycle/app');
 // const Observable = require('./product_cycle/observable');
 const main = require('./product_cycle/main');
-// // const observable = new Observable();
-// const producer = new main.Producer();
-// let observer = observable.subscribe(producer.isLast);
-// observable.fire('3 Owen Bartkowski');
-// console.log(observable);
+
 
 const executeOperation = new main.ExecuteOperation();
 executeOperation.execute();
