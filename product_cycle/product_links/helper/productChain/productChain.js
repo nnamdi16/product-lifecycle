876e5ad57dc2632bbd0facd297b8dd033740f0aa
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-06 10:52:04 +0100

    Remove instantiation of productFactory to reduce making the class tightly coupled

--- product_cycle/product_links/helper/productChain/productChain.js
+++ product_cycle/product_links/helper/productChain/productChain.js
@@ -1,6 +1,6 @@
 //Required node fs module.
 const fs = require('fs');
-
+const factory = require('../../helper/productChainFactory/productChainFactory');
 
 //Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
@@ -8,25 +8,7 @@ class ProductChain {
         this.product = product; 
         this.state = state;
 
-        //Instantiated the productChain Factory that generates objects depending on the state change category.
-        const observable = new main.Observable();
-        const productChainFactory = new main.ProductChainFactory();
-        const producer = new main.Producer();
-       
-        let observer = observable.subscribe(producer.isLast);
-        // console.log(p);
-       
-
-
-        //Register the various state class category.
-        productChainFactory.register('Producer',main.Producer);
-        productChainFactory.register('Retailer',main.Retailer);
-        productChainFactory.register('Consumer',main.Consumer);
-        productChainFactory.register('Recycler',main.Recycler);
-
-        observable.fire(this.state,this.product);
-        
-        this.currentState = productChainFactory.getProductLink(state);
+        this.currentState = factory.productChainFactory.getProductLink(state);
         
     }
 
@@ -38,9 +20,7 @@ class ProductChain {
 
 }
 
-// const productChain = new ProductChain();
-// console.log(productChain);
 
 
 module.exports = ProductChain;
-const main = require('../../../main');
+
