0d500b8c7cc95bf44d718feb28ea9175e91ba40e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-07 09:00:20 +0100

    Remove the asyn move method so as to decouple the class

--- product_cycle/product_links/producer/producer.js
+++ product_cycle/product_links/producer/producer.js
@@ -1,19 +1,37 @@
 const fs = require('fs');
+const transfer = require('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/product_links/helper/moveProduct/moveProduct.js');
 
 class Producer {
-    constructor() {}
-    async move(prop) {
-
-        let output = `${prop} Product moved from Producer to Retailer`;
-        fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt', '\n' + output, (err) => {
-            if (err) throw err;
-            new main.Retailer().move(prop);
-            // this.isLast(prop);
-            // console.log(sample);
-            console.log(`${prop} Product processed from Producer!`);
-            return;
-        });
+    constructor() {
+        // super(state,prop);
+        
+        // super(state,prop);
+        
+        
+        // const moveProduct = new MoveProduct();
+        // moveProduct.moveOutput(state,prop,output,'Retailer');
     }
+
+    move(state,prop) {
+        // let moveProduct = new MoveProduct();
+        let productMove = transfer.moveProduct.moveOutput(state,prop);
+        console.log(productMove);
+    }
+
+    
+    // async move(prop,state) {
+
+    //     let output = `${prop} Product moved from ${state} to Retailer`;
+    //     fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt', '\n' + output, (err) => {
+    //         if (err) throw err;
+    //         new main.Retailer().move(prop);
+    //         // this.isLast(prop);
+    //         // console.log(sample);
+    //         console.log(`${prop} Product processed from Producer!`);
+    //         return;
+    //     });
+    // }
+
 
     isLast(state,prop) {
         // console.log('The Prop', prop);
@@ -28,6 +46,8 @@ class Producer {
 
 }
 
+// const producer = new Producer();
+// console.log(producer.move('Producer','Lucas Mocha'));
 
 module.exports = Producer;
 const main = require('../../main');
