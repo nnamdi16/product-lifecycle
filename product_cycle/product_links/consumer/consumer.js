cfb5f4a64dd6029deb5faa46a59e7b73f709ed94
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-07 08:55:49 +0100

    Remove the asyn move method so as to decouple the class

--- product_cycle/product_links/consumer/consumer.js
+++ product_cycle/product_links/consumer/consumer.js
@@ -1,19 +1,31 @@
 const fs = require('fs');
+const transfer = require('../helper/moveProduct/moveProduct');
+class Consumer  {
+    constructor() {
+        // super(state,prop);
+        
+        
+        // const moveProduct = new MoveProduct();
+        // moveProduct.moveOutput(state,prop,output,'Retailer');
+    }
 
-class Consumer {
-    constructor(){  }
+    move(state,prop) {
+        let productMove = transfer.moveProduct.moveOutput(state,prop);
+        // console.log(productMove);
+    }
+
     
-    async move(prop) {
-        let output = `${prop} Product moved from Consumer to Recycler`;
-        fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
-            if(err) throw err;
-            new main.Recycler().move(prop);
-            console.log(`${prop} Product processed from Consumer!`);
-            return;
-        });
+    // async move(prop,state) {
+    //     let output = `${prop} Product moved from ${state} to Recycler`;
+    //     fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
+    //         if(err) throw err;
+    //         new main.Recycler().move(prop);
+    //         console.log(`${prop} Product processed from Consumer!`);
+    //         return;
+    //     });
 
         
-    }
+    // }
     
 }
 
