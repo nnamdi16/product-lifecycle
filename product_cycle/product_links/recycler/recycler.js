965405f539652f668a641349c7b8b019db070d51
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-07 09:00:36 +0100

    Remove the asyn move method so as to decouple the class

--- product_cycle/product_links/recycler/recycler.js
+++ product_cycle/product_links/recycler/recycler.js
@@ -1,17 +1,28 @@
-let fs = require('fs');
-
+const fs = require('fs');
+const transfer = require('../helper/moveProduct/moveProduct');
 class Recycler  {
-    constructor() { }
-    async move(prop) {
-        let output = `${prop} Product moved from Recycler to Producer`;
-        fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
-            if(err) throw err;
-            new main.Producer().move(prop);
-            console.log(`${prop} Product processed from Recycler!`);
-            return;
-        });
-         
+    constructor() {
+        
+        
+        // const moveProduct = new MoveProduct();
+        // moveProduct.moveOutput(state,prop,output,'Retailer');
     }
+
+    move(state,prop) {
+        let productMove = transfer.moveProduct.moveOutput(state,prop);
+        console.log(productMove);
+    }
+
+    // async move(prop,state) {
+    //     let output = `${prop} Product moved from ${state} to Producer`;
+    //     fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
+    //         if(err) throw err;
+    //         new main.Producer().move(prop);
+    //         console.log(`${prop} Product processed from Recycler!`);
+    //         return;
+    //     });
+         
+    // }
     
 }
 
--- product_cycle/product_links/retailer/retailer.js
+++ product_cycle/product_links/retailer/retailer.js
@@ -1,17 +1,32 @@
 const fs  = require('fs');
-class Retailer {
-    constructor() {   }
-    async move(prop) {
-
-        let output = `${prop} Product moved from Retailer to Consumer`;
-        fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
-            if(err) throw err;
-           new main.Consumer().move(prop);
-            console.log(`${prop} Product processed from Retailer to Consumer!`);
-            return;
-        });
+const transfer = require('../helper/moveProduct/moveProduct');
+class Retailer  {
+    constructor() {
+        
+        
+        // const moveProduct = new MoveProduct();
+        // moveProduct.moveOutput(state,prop,output,'Retailer');
     }
+
+    move(state,prop) {
+        
+        let productMove = transfer.moveProduct.moveOutput(state,prop);
+        console.log(productMove);
+    }
+
+    // async move(prop,state) {
+
+    //     let output = `${prop} Product moved from ${state} to Consumer`;
+    //     fs.appendFile('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/output-file.txt','\n' + output,(err) => {
+    //         if(err) throw err;
+    //        new main.Consumer().move(prop);
+    //         console.log(`${prop} Product processed from Retailer to Consumer!`);
+    //         return;
+    //     });
+    // }
 }
+// const retailer = new Retailer();
+// console.log(retailer);
 
 module.exports = Retailer;
 const main = require('../../main');
