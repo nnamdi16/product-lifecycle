9188bbe49bd5d467bbbbe768c6f9d88052ca67a0
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-07 08:58:40 +0100

    Refactor the productChain file, removing redundant lines of code

--- product_cycle/product_links/helper/productChain/productChain.js
+++ product_cycle/product_links/helper/productChain/productChain.js
@@ -1,26 +1,39 @@
 //Required node fs module.
 const fs = require('fs');
-const factory = require('../../helper/productChainFactory/productChainFactory');
-
+const factory = require('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/product_links/helper/productChainFactory/productChainFactory.js');
+// const MoveProduct = require('../moveProduct/moveProduct');
+// console.log('The Factory',factory);
 //Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
     constructor(state, product) {
         this.product = product; 
         this.state = state;
-
-        this.currentState = factory.productChainFactory.getProductLink(state);
+        // console.log('The Factory',factory);
+        
+       
+        
+        
         
     }
 
     //Method that ensures product flows from one state to the other.
     start() {
-        this.currentState.move(this.product);
-        return true;
+        // let moveProduct = new MoveProduct();
+        // moveProduct.moveOutput(this.state,this.product);
+        let currentState = factory.productChainFactory.getProductLink(this.state);
+        console.log('Now',currentState);
+        // currentState.move('Producer','Calista Na');
+        // console.log(this.product);
+        currentState.move(this.state,this.product);
+        // return true;
+        // console.log(this.currentState);
     }
 
 }
 
+// const productChain = new ProductChain();
 
 
 module.exports = ProductChain;
+const main = require('../../../main');
 
