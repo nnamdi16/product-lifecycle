5a3522c5e6fbc23d060e75ad7f17d0efb3273743
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-08 10:59:15 +0100

    Remove redundant lines of code

--- product_cycle/helper/productChain/productChain.js
+++ product_cycle/helper/productChain/productChain.js
@@ -1,5 +1,5 @@
 const factory = require('../productChainFactory/productChainFactory');
-const observer = require('../observable/observable').observer;
+
 //Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
     constructor(state, product) {
@@ -11,7 +11,7 @@ class ProductChain {
     //Method that ensures product flows from one state to the other.
     start() {
         let currentState = factory.productChainFactory.getProductLink(this.state);
-        currentState.move(this.state, this.product,observer.write);
+        currentState.move(this.state, this.product);
     }
 }
 
