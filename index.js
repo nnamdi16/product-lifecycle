1351fbc4f94c07229175234c44d592793beedad7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-08 08:39:49 +0100

    removed redundant file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const ExecuteOperation = require('./product_cycle/app');
+const observer = require('./product_cycle/helper/observable/observable').observer;
 
 
 const executeOperation = new ExecuteOperation();
+// executeOperation.execute(observer.read);
 executeOperation.execute();
 
