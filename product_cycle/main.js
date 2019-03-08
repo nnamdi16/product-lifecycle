7220c9ffa151b1a9edb4d19dbeca068f30c4650e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-08 08:53:32 +0100

    Change file path for files whose directory were changed

--- product_cycle/main.js
+++ product_cycle/main.js
@@ -3,11 +3,11 @@ module.exports.Consumer = require('/Users/nnamdinwabuokei/Documents/Decagon/Inst
 module.exports.Producer = require('../product_cycle/product_links/producer/producer');
 module.exports.Retailer = require('../product_cycle/product_links/retailer/retailer');
 module.exports.Recycler = require('../product_cycle/product_links/recycler/recycler');
-module.exports.ProductChain = require('./product_links/helper/productChain/productChain');
+module.exports.ProductChain = require('./helper/productChain/productChain');
 module.exports.ExecuteOperation = require('./app');
-module.exports.ProductChainFactory = require('./product_links/helper/productChainFactory/productChainFactory');
-module.exports.MoveProduct = require('../product_cycle/product_links/helper/moveProduct/moveProduct');
-module.exports.Observable = require('./product_links/helper/observable/observable');
+module.exports.ProductChainFactory = require('./helper/productChainFactory/productChainFactory');
+module.exports.MoveProduct = require('./helper/moveProduct/moveProduct');
+module.exports.Observable = require('./helper/observable/observable');
 
 
 
