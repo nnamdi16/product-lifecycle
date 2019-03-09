f4bb2f9dd57c019976bdc2291f24ddbe4d48aab7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-09 11:34:44 +0100

    Added instance of the productChain Class to access it s method and decouple ExecutionOperation class

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 const ExecuteOperation = require('./product_cycle/app');
+const ProductChain = require('./product_cycle/helper/productChain/productChain');
 const observer = require('./product_cycle/helper/observable/observable').observer;
 
 
 const executeOperation = new ExecuteOperation();
+const productChain = new ProductChain();
 // executeOperation.execute(observer.read);
-executeOperation.execute(observer.read);
+let output = executeOperation.run(observer.read);
+console.log(output);
 
+// let value = observer.read;
+// console.log(value);
