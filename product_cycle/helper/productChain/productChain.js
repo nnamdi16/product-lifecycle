253afcfcab78c9a6a7676cca96a7b43e4e26551e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-09 11:39:05 +0100

    Refactor code to allow for testing

--- product_cycle/helper/productChain/productChain.js
+++ product_cycle/helper/productChain/productChain.js
@@ -2,16 +2,16 @@ const factory = require('../productChainFactory/productChainFactory');
 
 //Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
-    constructor(state, product) {
-        this.product = product;
-        this.state = state;
+    constructor() {
+        // this.product = product;
+        // this.state = state;
 
     }
 
     //Method that ensures product flows from one state to the other.
-    start() {
-        let currentState = factory.productChainFactory.getProductLink(this.state);
-        currentState.move(this.state, this.product);
+    start(state,prop) {
+        let currentState = factory.productChainFactory.getProductLink(state);
+        currentState.move(state, prop);
     }
 }
 
