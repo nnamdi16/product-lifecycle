b44eadf901d2fd9a08a39bda6af931d5c0d24114
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-09 11:38:50 +0100

    Refactor code to allow for testing

--- product_cycle/helper/moveProduct/moveProduct.js
+++ product_cycle/helper/moveProduct/moveProduct.js
@@ -4,14 +4,15 @@ const factory = require('../productChainFactory/productChainFactory');
 class MoveProduct {
     constructor() {}
 
-    async moveOutput(state, prop,url) {
+    async moveOutput(state, prop, url) {
         let result = factory.productChainFactory.getNextProductLink(super.productChainFactory, state);
         let output = `${prop} Product moved from ${state} to ${result[0]}`;
+        // console.log(output);
+        
 
-        console.log(output);
-
-    fs.appendFile(url, '\n' + output, (err) => {
+        fs.appendFile(url, '\n' + output, (err) => {
             if (err) throw err;
+            // console.log(output);
             let nextState = factory.productChainFactory.getProductLink(result[0]);
             nextState.move(result[0], prop);
 
