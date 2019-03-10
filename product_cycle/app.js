df8ffabf1d8d927cfb23a83a8fbcf363df115994
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-10 21:24:15 +0100

    Refactor the ExecuteOperation class, decoupling it by adding the execute method for reading from the file, removeImpunity method for cleanning the file and assign state method for assigning states to each product

--- product_cycle/app.js
+++ product_cycle/app.js
@@ -2,108 +2,62 @@ const fs = require('fs');
 const ProductChain = require('./helper/productChain/productChain');
 const observer = require('./helper/observable/observable').observer;
 // console.log(observer.check);
-
+let counter = 0;
 class ExecuteOperation {
 	constructor() {}
 	execute(url) {
-		const data = fs.readFileSync(url,
+		let data = fs.readFileSync(url,
 			'utf8');
-		return data;	
-		
+		return data;
 	}
+	removeImpunity(data) {
+		let inputFile = data.split("\n").map(file => {
+			let result = file.replace(/[^\w\s]/gm, "").replace(/\s\s+/gm, " ").trim();
+			return result;
+		});
 
-	run(url) {
-		let product = this.execute(url);
-		let array = product.toString().split('\n');
-		let output = this.removeImpunity(array);
-		return output;
-		
-
-	}
-	removeImpunity(array) {
-		let counter = 0,assignProduct;
-		for (let index = 0; index < array.length; index++) {
-			let output;
-			let result = array[index].replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\t+\r?\n|\r]/gm, ' ').replace(/ +/gm, ' ');
-			if (!result.match(/^\s*$/gm)) {
-
-				output = result.trim().split(' ');
-				counter++;
-				 assignProduct = this.assignState(output, counter);
-				
-				// console.log(assignProduct);
+		for (let i = inputFile.length - 1; i >= 0; i--) {
+			if (!/[^\s]/.test(inputFile[i])) {
+				inputFile.splice(i, 1);
 			}
-
 		}
-		return assignProduct;
-		
+
+		for (let i = 0; i < inputFile.length; i++) {
+			if (inputFile[i].split(" ").length === 2) {
+				inputFile[i] = inputFile[i] + " Producer";
+			}
+		}
+
+		return inputFile;
+
 	}
 
-	assignState(product, counter) {
-		let prop = `${counter} ${product[0]} ${product[1]}`;
-		let state = `${product[2]}`;
-		if (product.length === 3) {
-			state = `${product[2]}`;
-			prop = `${counter} ${product[0]} ${product[1]}`;
-			return [state, prop];
-			// const productChain = new ProductChain(state, prop);
-			// productChain.start();
-		} else {
-			state = 'Producer';
-			prop = `${counter} ${product[0]} ${product[1]}`;
-			return [state, prop];
-			// const productChain = new ProductChain('Producer', prop);
-			// productChain.start();
+	assignState(url) {
+		let output = this.execute(url);
+		let product = this.removeImpunity(output);
+		let productObj;
+		for (let index = 0; index < product.length; index++) {
+			let element = product[index].split(' ');
+			let counter = index + 1;
+			productObj = {
+				state: `${element[2]}`,
+				prop: `${counter} ${element[0]} ${element[1]}}`
+			};
+			return productObj;
+
 		}
+
 	}
-	// execute(array) {
-	// 	for (let index = 0; index < array.length; index++) {
-
-
-	// 	}
-
-	// 	
-	// }
-	// execute(url) {
-	// 	try {
-	// 		const data = fs.readFileSync(url,
-	// 			'utf8'
-	// 		);
-	// 		let array = data.toString().split('\n');
-	// 		let count = 0;
-	// 		for (let index = 0; index < array.length; index++) {
-	// 			let result = array[index]
-	// 				.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/\t+\r?\n|\r]/gm, ' ')
-	// 				.replace(/ +/gm, ' ');
-	// 			if (!result.match(/^\s*$/gm)) {
-	// 				let product = result.trim().split(' ');
-	// 				count++;
-	// 				let prop = `${count} ${product[0]} ${product[1]}`;
-	// 				let state = `${product[2]}`;
-	// 				if (product.length === 3) {
-	// 					const productChain = new ProductChain(state, prop);
-	// 					productChain.start();
-	// 				} else {
-	// 					const productChain = new ProductChain('Producer', prop);
-	// 					productChain.start();
-	// 				}
-	// 			}
-
-	// 		}
-	// 		observer.check = true;
-
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 	}
-	// }
-
 }
+
 // const executeOperation = new ExecuteOperation();
-// let url = 'data/read';
-// // let output = executeOperation.removeImpunity(['///\tBack Right']);
-// let output = executeOperation.run(url);
+// // // // let url = 'data/read';
+// let url = '/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/data/product-Input.txt';
+// let data = executeOperation.execute(url);
+// let output = executeOperation.removeImpunity(data);
+// // console.log(executeOperation.assignState(url));
 // console.log(output);
-// console.log(executeOperation.execute('./data/product-Input.txt')); 
+// // console.log(executeOperation.execute('./data/product-Input.txt')); 
 
 
 module.exports = ExecuteOperation;
