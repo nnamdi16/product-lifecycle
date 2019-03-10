64d0c454da1328de35dbe403ef973d53e108137c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-10 21:27:27 +0100

    Refactor the consumer.test.js

--- product_cycle/product_links/consumer/consumer.test.js
+++ product_cycle/product_links/consumer/consumer.test.js
@@ -1,28 +1,29 @@
-
 const fs = require('fs');
 const Consumer = require('./consumer');
 const transfer = require('../../helper/moveProduct/moveProduct');
+const ExecuteOperation = require('../../app');
 
-describe("Check if the instance of the consumer class is created",() => {
-    it("Should create an instance of Consumer class",() => {
+describe("Check if the instance of the consumer class is created", () => {
+    it("Should create an instance of Consumer class", () => {
         const consumer = new Consumer();
         const result = {};
         expect(consumer).toEqual(result);
     });
-  
+
     it("Check if the Object created is an instance of the Consumer  Constructor", () => {
         const consumer = new Consumer();
         expect(consumer).toBeInstanceOf(Consumer);
-      });
+    });
 
-    
+
 });
 
-// const consumer = new Consumer();
-
-//     test(`move 'test' to equal 'TEST'`,() =>{
-//         return consumer.move('test').then((state,prop) =>{
-//             expect((state,prop)).toBe('TEST');
-//         });
+// describe('Check whether removeImpunity cleaned all the impunities', () => {
+//     it('Should check for impunities and remove it ', () => {
+//         let consumer = new Consumer();
+      
+//        let result = 'Consumer ' 
+//         expect(result).toBe(consumer.move('Consumer','Read Shan') );
 //     });
 
+// });
