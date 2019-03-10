f199cdabf42e50cdac2a36e6e5c47ecdad59788a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-10 21:26:36 +0100

    Refactor the productChain.js file

--- product_cycle/helper/productChain/productChain.js
+++ product_cycle/helper/productChain/productChain.js
@@ -2,16 +2,16 @@ const factory = require('../productChainFactory/productChainFactory');
 
 //Created a productChain class that instantiate a productChain during state change.
 class ProductChain {
-    constructor() {
-        // this.product = product;
-        // this.state = state;
+    constructor(state,product) {
+        this.product = product;
+        this.state = state;
 
     }
 
     //Method that ensures product flows from one state to the other.
-    start(state,prop) {
-        let currentState = factory.productChainFactory.getProductLink(state);
-        currentState.move(state, prop);
+    start() {
+        let currentState = factory.productChainFactory.getProductLink(this.state);
+        currentState.move(this.state, this.product);
     }
 }
 
