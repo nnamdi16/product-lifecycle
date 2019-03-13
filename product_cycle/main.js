dc7eb0800e3263c413dcad9331857ae375fdbe54
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-13 18:57:05 +0100

    Change the name of the imported class from ExecuteOperation to ProductFlow to match the name of its folder and file

--- product_cycle/main.js
+++ product_cycle/main.js
@@ -1,10 +1,10 @@
 //Export classes  to  main.js to avoid circular dependency
-module.exports.Consumer = require('/Users/nnamdinwabuokei/Documents/Decagon/Institute/checkpoint/product-lifecycle/product_cycle/product_links/consumer/consumer.js');
+module.exports.Consumer = require('../product_cycle/product_links/consumer/consumer.js');
 module.exports.Producer = require('../product_cycle/product_links/producer/producer');
 module.exports.Retailer = require('../product_cycle/product_links/retailer/retailer');
 module.exports.Recycler = require('../product_cycle/product_links/recycler/recycler');
 module.exports.ProductChain = require('./helper/productChain/productChain');
-module.exports.ExecuteOperation = require('./helper/productFlow/productFlow');
+module.exports.ProductFlow = require('./helper/productFlow/productFlow');
 module.exports.ProductChainFactory = require('./helper/productChainFactory/productChainFactory');
 module.exports.MoveProduct = require('./helper/moveProduct/moveProduct');
 module.exports.Observable = require('./helper/observable/observable');
