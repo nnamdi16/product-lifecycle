ae5292d5b589ab1e1f517793f328d12f03b6d61c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-13 19:00:06 +0100

    Change the name of the class to ProductFlow with reference to its changed name in productFlow.js

--- product_cycle/helper/productFlow/productFlow.test.js
+++ product_cycle/helper/productFlow/productFlow.test.js
@@ -1,19 +1,19 @@
 //Import  fs and ExecuteOperation class.
 const fs = require('fs'); 
-const ExecuteOperation = require('./productFlow');
+const ProductFlow = require('./productFlow');
 
 
 //Test case to check for the instance of ExecuteOperation class
 describe("Check if the instance of the ExecuteOperation class is created", () => {
     it("Should create an instance of the ExecuteOperation class", () => {
-        const executeOperation = new ExecuteOperation();
+        const productFlow = new ProductFlow();
         const result = {};
-        expect(executeOperation).toEqual(result);
+        expect(productFlow).toEqual(result);
     });
 
     it("Check if the Object created is an instance of the Consumer  Constructor", () => {
-        const executeOperation = new ExecuteOperation();
-        expect(executeOperation).toBeInstanceOf(ExecuteOperation);
+        const productFlow = new ProductFlow();
+        expect(productFlow).toBeInstanceOf(ProductFlow);
     });
 
 
@@ -22,11 +22,11 @@ describe("Check if the instance of the ExecuteOperation class is created", () =>
 //Test case to check if the execute method reads the file based on the url
 describe('Check whether files are read from the directory selected ', () => {
     it('Should check for impunities and remove it ', () => {
-        let executeOperation = new ExecuteOperation();
+        let productFlow = new ProductFlow();
         let url = 'product_cycle/data/read';
         fs.writeFileSync(url, '///Read Shan Consumer//***');
-        executeOperation.execute(url);
-        expect(executeOperation.execute(url)).toBe('///Read Shan Consumer//***');
+        productFlow.execute(url);
+        expect(productFlow.execute(url)).toBe('///Read Shan Consumer//***');
         fs.unlinkSync(url);
     });
 
@@ -35,23 +35,23 @@ describe('Check whether files are read from the directory selected ', () => {
 //Test case to check if the removeImpunity method cleans the impunities in the file.
 describe('Check whether removeImpunity cleans all impunities', () => {
     it('Should check for impunities and remove it ', () => {
-        let executeOperation = new ExecuteOperation();
+        let productFlow = new ProductFlow();
         let url = 'product_cycle/data/read';
         let result = ['Read Shan Consumer', 'Sample Read Up'];
         fs.appendFileSync(url, '///Read Shan Consumer\\\\\n\nSample Read Up');
-        let data = executeOperation.execute(url);
-        let output = executeOperation.removeImpunity(data);
+        let data = productFlow.execute(url);
+        let output = productFlow.removeImpunity(data);
         expect(output).toEqual(result);
         fs.unlinkSync(url);
     });
 
     it('Should check for impunities and remove it ', () => {
-        let executeOperation = new ExecuteOperation();
+        let productFlow= new ProductFlow();
         let url = 'product_cycle/data/read';
         let result = ['Read Shan Producer'];
         fs.writeFileSync(url, '///Read Shan \\\\');
-        let data = executeOperation.execute(url);
-        let output = executeOperation.removeImpunity(data);
+        let data = productFlow.execute(url);
+        let output = productFlow.removeImpunity(data);
         expect(output).toEqual(result);
         fs.unlinkSync(url);
     });
@@ -63,9 +63,9 @@ describe('Check whether removeImpunity cleans all impunities', () => {
 describe('Assigns states to the array of products', () => {
     it('Should assigns states to the array of products without an assigned state', () => {
         let url = 'product_cycle/data/read';
-        let executeOperation = new ExecuteOperation();
+        let productFlow = new ExecuteOperation();
         fs.writeFileSync(url, '///Read Shan Consumer\\\\');
-        let output = executeOperation.assignState(url);
+        let output = productFlow.assignState(url);
         let result = {
             state: 'Consumer',
             prop: '1 Read Shan'
